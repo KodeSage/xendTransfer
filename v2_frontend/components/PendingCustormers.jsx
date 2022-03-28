d417306581983be715b9061cc77186e648772525
James Harrison <somtochukwujames2000@gmail.com>
2022-03-28 10:41:59 +0100

    refactor: Refactored Components, utilis , contexts and pages folder

---
+++ v2_frontend/components/PendingCustormers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PendingCustormers() {
+  return (
+    <div>PendingCustormers</div>
+  )
+}
---
+++ v2_frontend/pages/admin.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { PendingCustormers, ConfirmedCustormers, Navbar, Footer } from '../components';
+
+export default function admin() {
+  return (
+      <div className="min-h-screen">
+          <div className="gradient-bg-welcome">
+              <Navbar />
+              <PendingCustormers />
+              <ConfirmedCustormers />
+          </div>
+          <div className="gradient-bg-services">
+              <Footer />
+          </div>
+    </div>
+  )
+}
